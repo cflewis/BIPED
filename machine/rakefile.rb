4aefc10c89b21a40f630c7ba077ec69a3d93c8aa
Chris Lewis <chris@chris.to>
2010-03-16 01:22:15 -0700

    Converted to using gringo and clasp, added option to change models generated

--- machine/rakefile.rb
+++ machine/rakefile.rb
@@ -5,10 +5,16 @@ CLEAN.include('problem', 'solutions', 'rules.lp')
 LPARSE_FILES = FileList['ec.lp', 'engine.lp', 'rules.lp', 'context.lp']
 PROLOG = 'swipl'
 
-if ENV['file'].nil?
-    GAME_FILE = 'script.pl'
-else
+if not ENV['file'].nil?
     GAME_FILE = ENV['file']
+else
+    GAME_FILE = 'script.pl'
+end
+
+if not ENV['models'].nil?
+    MODELS = ENV['models']
+else
+    MODELS = '10'
 end
 
 task :default => 'solution'
@@ -18,12 +24,12 @@ task 'rules.lp' => GAME_FILE do
 end
 
 task 'problem' => LPARSE_FILES do
-    sh "lparse --true-negation -c t_max=5 #{LPARSE_FILES.to_s()} " +
+    sh "gringo -c t_max=5 #{LPARSE_FILES.to_s()} " +
         "| pv -N grounding > problem"
 end
 
 task 'solution' => ['problem'] do
-    sh %{pv -N solving problem | clasp -n 10 > solution} do | ok, status |
+    sh %{pv -N solving problem | clasp -n #{MODELS} > solution} do | ok, status |
         if status.exitstatus == 10 then
             puts "Satisfied"
         else
